4abbffe110a43c54a11c03164af331257f544337
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-03-17 16:11:43 +0100

    add width to pros of image

--- src/components/CustomImage/CustomImage.tsx
+++ src/components/CustomImage/CustomImage.tsx
@@ -5,12 +5,22 @@ interface Props {
     src: string;
     alt: string;
     tooltipText?: string;
-}
+    width?: string;
+}   
 
-export const CustomImage: React.FC<Props> = ({ src, alt, tooltipText}) => {
+export const CustomImage: React.FC<Props> = ({ src, alt, tooltipText, width}) => {
     return <Tooltip title={tooltipText}>
         <span>
-            <img src={src} className="headingImage" alt={alt}/>;
+            <img 
+                src={src} 
+                className="headingImage" 
+                alt={alt} 
+                style={
+                    width
+                    ? { width }
+                    : {}
+                }
+            />
         </span>
     </Tooltip>
 }
