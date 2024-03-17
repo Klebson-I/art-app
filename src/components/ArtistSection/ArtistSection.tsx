0dfc997e8b258daec77c16d93ddec9a1997aa80f
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-03-17 16:10:27 +0100

    create components to generate sections of artists

--- src/components/ArtistSection/ArtistSection.tsx
+++ src/components/ArtistSection/ArtistSection.tsx
@@ -1,3 +1,36 @@
-export const ArtistSection = () => {
+import { Box } from "@mui/material";
+import './style.css';
+import { CustomImage } from "../CustomImage/CustomImage";
+import { useEffect, useRef, useState } from "react";
+import { useIsElementVisible } from "../../hooks/useIsElementVisible";
 
+interface Props {
+    name: string;
+    information: string;
+    artistMainColor: string;
+    imgSrc: string;
+    exampleImageTitle: string;
+}
+
+
+export const ArtistSection: React.FC<Props> = ({ name, information, artistMainColor, imgSrc, exampleImageTitle }) => {
+    const ref = useRef<Element|null>(null);
+    const [additionalClassHeader, setAdditionalClassHeader] = useState<string>('');
+    const [additionalClassInfo, setAdditionalClassInfo] = useState<string>('');
+    const isVisible = useIsElementVisible(ref);
+
+    useEffect(() => {
+        if(isVisible) {
+            setAdditionalClassHeader('animateSlideOneSec');
+            setAdditionalClassInfo('animateSlideTwoSec');
+        }
+    }, [isVisible]);
+    
+    return <Box className="painterBox" sx={{backgroundColor: `${artistMainColor}`}} ref={ref}>
+        <CustomImage src={imgSrc} alt={name} tooltipText={exampleImageTitle} width="20rem"/>
+        <div className="informationContainer">
+            <h1 className={`painterName ${additionalClassHeader}`}>{`> ${name}`}</h1>
+            <p className={`painterInformation ${additionalClassInfo}`}>{information}</p>
+        </div>
+    </Box>
 };
---
+++ src/components/SectionGenerator/SectionGenerator.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ArtistSection } from "../ArtistSection/ArtistSection"
+import { paintersSections } from "./constants"
+
+export const SectionGenerator: React.FC<{}> = () => {
+    const sections = paintersSections.map(({
+        artistMainColor,
+        exampleImageTitle,
+        imgSrc,
+        information,
+        name,
+    }, index) => (
+        <ArtistSection 
+            artistMainColor={artistMainColor}
+            exampleImageTitle={exampleImageTitle}
+            imgSrc={imgSrc}
+            information={information}
+            name={name}
+            key={index}
+        />
+    ))
+
+    return <>
+        {sections}
+    </>;
+}
