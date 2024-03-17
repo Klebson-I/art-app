7aba54d045ee4dc96ce3211da9285541efca46e3
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-03-17 17:08:07 +0100

    provide context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StartHeaderTab } from './components/StartHeaderTab/StartHeaderTab';
 import { SectionGenerator } from './components/SectionGenerator/SectionGenerator';
+import { ScrollState } from './context/ScrollContext';
 
 function App() {
   return (
     <div className="App">
-      <StartHeaderTab />
-      <SectionGenerator />
+      <ScrollState>
+        <StartHeaderTab />
+        <SectionGenerator />
+      </ScrollState>
     </div>
   );
 }
