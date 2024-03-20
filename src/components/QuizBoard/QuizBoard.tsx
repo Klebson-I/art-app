8475f23cbc0408204c80e8df2e9fd6d129f6f580
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-03-20 20:07:47 +0100

    add styles

--- src/components/QuizBoard/QuizBoard.tsx
+++ src/components/QuizBoard/QuizBoard.tsx
@@ -2,6 +2,7 @@ import {  Button, Grid } from "@mui/material"
 import { getQuestionSet } from "./utils"
 import { useEffect, useState } from "react";
 import { QuestionsAssignWithOptionsInterface, QuestionsAssignsWithOptionsListType } from "./types";
+import './style.css';
 
 export const QuizBoard = () => {
     const [quiz, setQuiz] = useState<QuestionsAssignsWithOptionsListType>([]);
@@ -24,15 +25,15 @@ export const QuizBoard = () => {
     }, [actualQuestionIndex, quiz]);
 
 
-    return <Grid container spacing={12}>
-        <Grid item spacing={6}>
-            <img src={actualQuestion?.image} alt="" style={{ width: '300px', height: 'auto'}}/>
+    return <Grid container spacing={12} className="gridContainer">
+        <Grid item spacing={12} className="imageGrid">
+            <img src={actualQuestion?.image} alt="" className="questionImage"/>
         </Grid>
-        <Grid item spacing={6}>
+        <Grid item spacing={12} className="answerGrid">
             {
                 actualQuestion?.options.map((option) => <p>{option}</p>)
             }
+            <Button onClick={incrementQuestion} variant='outlined'>Next Question</Button>
         </Grid>
-        <Button onClick={incrementQuestion} variant='outlined'>Next Question</Button>
     </Grid>
 }
