b2e30943dbe6b3345d11e2a1c018aba92398cd40
Łukasz Klęba <lukasz.kleba99@onet.pl>
2024-03-20 19:49:48 +0100

    display QuizBoard

--- src/Pages/Quiz/Quiz.tsx
+++ src/Pages/Quiz/Quiz.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material"
 import './style.css';
 import { RoundIndicator } from "../../components/RoundIndicator/RoundIndicator";
+import { QuizBoard } from "../../components/QuizBoard/QuizBoard";
 
 export const Quiz = () => {
     return <Box className="quizPage">
         <RoundIndicator />
+        <QuizBoard />
     </Box>
 }
