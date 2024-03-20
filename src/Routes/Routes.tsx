436cee0766ca87ee2165dfa68367bdb1c0be4187
lkleba <lukasz.kleba.ext@nokia.com>
2024-03-20 15:17:06 +0100

    add react router

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,11 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import App from "../App";
+
+
+export const GlobalRoutes = () => {
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />}/>
+        </Routes>
+    </BrowserRouter>
+};
